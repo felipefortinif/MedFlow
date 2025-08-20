001e2109b2708c7544c6bb6e3738ca41b893609d
felipefortinif <felipefortinif@gmail.com>
2025-08-19 21:28:22 -0300

    implementado um front inutil, porém que ajuda a entender a estrutura do angular

--- Site/frontend/frontend/src/app/app.routes.ts
+++ Site/frontend/frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', redirectTo: '/esquerda', pathMatch: 'full' },
+	{
+		path: 'esquerda',
+		loadComponent: () => import('./left-page/left-page.component').then((m) => m.LeftPageComponent),
+	},
+	{
+		path: 'direita',
+		loadComponent: () => import('./right-page/right-page.component').then((m) => m.RightPageComponent),
+	},
+];
