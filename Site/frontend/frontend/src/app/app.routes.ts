d9f687aa8ea7ac762f708277bd7b636a67aa5f58
felipefortinif <felipefortinif@gmail.com>
2025-08-25 20:18:02 -0300

    criação da tela_teste com o codigo adaptado do template do django (está dando erro 404)

--- Site/frontend/frontend/src/app/app.routes.ts
+++ Site/frontend/frontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { TelaPrincipalComponent } from './tela_principal/tela_principal.component';
 import { GravarAudioComponent } from './gravar_audio/gravar_audio.component';
 import { ProntuarioComponent } from './prontuario/prontuario.component';
+import { TelaTeste } from './teste/tela_teste.component';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'tela-principal', pathMatch: 'full' },
 	{ path: 'tela-principal', component: TelaPrincipalComponent },
 	{ path: 'gravar-audio', component: GravarAudioComponent },
 	{ path: 'prontuario', component: ProntuarioComponent },
+	{ path: 'tela-teste', component: TelaTeste},
 	{ path: '**', redirectTo: 'tela-principal' }
 ];
